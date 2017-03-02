65422969a1540e1b6f5f9248b277b19304e63ecc
csmets <clyde.smets@gmail.com>
2017-03-02 18:10:18 +1000

    Updated to change project path via CLI arg

--- digest.py
+++ digest.py
@@ -4,6 +4,13 @@ from datetime import datetime
 import json
 import subprocess
 import requests
+import argparse
+
+# CLI argument parser for setting path of files to write to.
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--path', type=str, default='./', help='Path to project.')
+
+dir_path = parser.parse_args()
 
 def read_lines(func, lines, return_val, start_at=0):
 
@@ -211,28 +218,28 @@ rirs = gather_rir_stats()
 global_results = global_stats(rirs)
 
 # Write Global Stats
-write_daily_digest('README.md', global_results)
-write_json('./archives/global-delegations.json', global_results)
+write_daily_digest(dir_path + 'README.md', global_results)
+write_json(dir_path + 'archives/global-delegations.json', global_results)
 
 # AFRINIC Digest
-write_daily_digest('./archives/AFRINIC/README.md', rirs['afrinic'])
-write_json('./archives/AFRINIC/afrinic-delegations.json', rirs['afrinic'])
+write_daily_digest(dir_path + 'archives/AFRINIC/README.md', rirs['afrinic'])
+write_json(dir_path + 'archives/AFRINIC/afrinic-delegations.json', rirs['afrinic'])
 
 # APNIC Digest
-write_daily_digest('./archives/APNIC/README.md', rirs['apnic'])
-write_json('./archives/APNIC/apnic-delegations.json', rirs['apnic'])
+write_daily_digest(dir_path + 'archives/APNIC/README.md', rirs['apnic'])
+write_json(dir_path + 'archives/APNIC/apnic-delegations.json', rirs['apnic'])
 
 # ARIN Digest
-write_daily_digest('./archives/ARIN/README.md', rirs['arin'])
-write_json('./archives/ARIN/arin-delegations.json', rirs['arin'])
+write_daily_digest(dir_path + 'archives/ARIN/README.md', rirs['arin'])
+write_json(dir_path + 'archives/ARIN/arin-delegations.json', rirs['arin'])
 
 # LACNIC Digest
-write_daily_digest('./archives/LACNIC/README.md', rirs['lacnic'])
-write_json('./archives/LACNIC/lacnic-delegations.json', rirs['lacnic'])
+write_daily_digest(dir_path + 'archives/LACNIC/README.md', rirs['lacnic'])
+write_json(dir_path + 'archives/LACNIC/lacnic-delegations.json', rirs['lacnic'])
 
 # RIPE NCC Digest
-write_daily_digest('./archives/RIPE_NCC/README.md', rirs['ripe'])
-write_json('./archives/RIPE_NCC/ripencc-delegations.json', rirs['ripe'])
+write_daily_digest(dir_path + 'archives/RIPE_NCC/README.md', rirs['ripe'])
+write_json(dir_path + 'archives/RIPE_NCC/ripencc-delegations.json', rirs['ripe'])
 
 # Add updated daily digest
 print("Git adding new digest...")
